40b227b2809cdf62f28ce57d8e24539b94a5030e
megustaEevee <71437195+megustaEevee@users.noreply.github.com>
2023-01-08 21:09:03 +0900

    ログイン機能追加

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -9,7 +9,6 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -39,7 +38,6 @@ def register():
 
     return render_template('auth/register.html')
 
-
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -65,7 +63,6 @@ def login():
 
     return render_template('auth/login.html')
 
-
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
@@ -77,12 +74,10 @@ def load_logged_in_user():
             'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
-
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
 
 def login_required(view):
     @functools.wraps(view)
